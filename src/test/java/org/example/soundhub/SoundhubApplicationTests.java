e5b59fb5546e8ec24e6aa79d0309d62072da5851
Grigor Hovsepyan <theo.grigor787@gmail.com>
2026-02-26 01:10:24 +0400

    Initial commit

---
+++ src/test/java/org/example/soundhub/SoundhubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.soundhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
